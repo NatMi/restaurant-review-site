494e36ab1a258a3e65707e7e7731345ecb6fc054
Natalia <nat.mietus@gmail.com>
2020-01-19 23:44:53 +0000

    Separated RestaurantItem component from RestaurantSidebar

---
+++ src/Components/RestaurantItem.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "../Styles/restaurantSidebar.css";
+import googleMapsKey from "../Data/googleMapsKey.json";
+
+class RestaurantItem extends Component {
+  streetViewUrl() {
+    return (
+      "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" +
+      this.props.restaurant.geometry.location.lat() +
+      "," +
+      this.props.restaurant.geometry.location.lng() +
+      "&fov=80&heading=70&pitch=0&key=" +
+      googleMapsKey.apiKey
+    );
+  }
+  render() {
+    return (
+      /*restaurant card: */
+      <div className="restaurantCard" key={this.props.restaurant.place_id}>
+        <div className="restaurantCardInfo">
+          <h4 className="restaurantName">{this.props.restaurant.name}</h4>
+          <p className="restaurantRating">
+            {this.props.restaurant.rating}/5 out of{" "}
+            {this.props.restaurant.user_ratings_total} reviews
+          </p>
+          <p className="restaurantAddress">{this.props.restaurant.vicinity}</p>
+          <div className="restaurantBtnContainer">
+            <button className="btnRestaurantReviews">Read reviews</button>{" "}
+            <button className="btnAddRestaurantReviews">Add review</button>
+          </div>
+        </div>
+
+        <div className="restaurantCardStreetView">
+          <img
+            alt={this.props.restaurant.name}
+            className="streetViewImg"
+            src={this.streetViewUrl()}
+          ></img>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RestaurantItem;
--- src/Components/RestaurantSidebar.js
+++ src/Components/RestaurantSidebar.js
@@ -1,58 +1,44 @@
 import React, { Component } from "react";
-import "../Styles/restaurantItem.css";
-import googleMapsKey from "../Data/googleMapsKey.json";
+import RestaurantItem from "./RestaurantItem.js";
+import "../Styles/restaurantSidebar.css";
 
 class RestaurantSidebar extends Component {
-  componentDidUpdate() {
-    if (this.props.getVisibleRestaurants.length <= 0) {
-      return <h3>Sorry, no restaurants found in this area.</h3>;
-    } else {
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentRestaurantList: []
+    };
+    // this.componentDidUpdate = this.componentDidUpdate.bind(this);
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    // only update if the data has changed
+    if (prevProps.getVisibleRestaurants !== this.props.getVisibleRestaurants) {
+      this.setState({
+        currentRestaurantList: this.props.getVisibleRestaurants
+      });
+    }
+    console.log("currentRest:" + this.state.currentRestaurantList.length);
+  }
+
   render() {
-    return (
-      <div id="restaurantList">
-        <h3>Restaurants found: {this.props.getVisibleRestaurants.length}</h3>
+    if (this.state.currentRestaurantList.length <= 0) {
+      return (
+        <div id="restaurantList">
+          <h3>No restaurants found in this area.</h3>
+        </div>
+      );
+    } else {
+      return (
+        <div id="restaurantList">
+          <h3>Restaurants found: {this.state.currentRestaurantList.length}</h3>
 
-        {this.props.getVisibleRestaurants.map(restaurant => {
-          let streetViewUrl =
-            "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" +
-            restaurant.geometry.location.lat() +
-            "," +
-            restaurant.geometry.location.lng() +
-            "&fov=80&heading=70&pitch=0&key=" +
-            googleMapsKey.apiKey;
-
-          return (
-            /*restaurant card: */
-            <div className="restaurantCard" key={restaurant.place_id}>
-              <div className="restaurantCardInfo">
-                <h4 className="restaurantName">{restaurant.name}</h4>
-                <p className="restaurantRating">
-                  {restaurant.rating}/5 out of {restaurant.user_ratings_total}{" "}
-                  reviews
-                </p>
-                <p className="restaurantAddress">{restaurant.vicinity}</p>
-                <div className="restaurantBtnContainer">
-                  <button className="btnRestaurantReviews">Read reviews</button>{" "}
-                  <button className="btnAddRestaurantReviews">
-                    Add review
-                  </button>
-                </div>
-              </div>
-
-              <div className="restaurantCardStreetView">
-                <img
-                  alt={restaurant.name}
-                  className="streetViewImg"
-                  src={streetViewUrl}
-                ></img>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
+          {this.state.currentRestaurantList.map(restaurant => (
+            <RestaurantItem restaurant={restaurant} />
+          ))}
+        </div>
+      );
+    }
   }
 }
 export default RestaurantSidebar;
